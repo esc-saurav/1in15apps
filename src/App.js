09b0c362de0ac5a334a0d08164afe5c999fde8bb
Saurav Panthi <sauravpanthiedu@gmail.com>
2023-03-02 21:47:01 +0545

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import { BrowserRouter, Routes } from "react-router-dom";
-// import Birthday from "./components/birthdayReminder/Birthday";
-// import Tours from "./components/tours/Tours";
-// import Review from "./components/reviewsinfo/Review";
-// import Qna from "./components/question&answer/Qna";
-// import Menu from "./components/menu/Menu";
-// import Tabs from "./components/tabs/Tabs";
-// import Slider from "./components/slider/Slider";
-// import Lorem from "./components/lorem/Lorem";
-// import Color from "./components/color-generator/Color";
-// import Grocery from "./components/grocery/Grocery";
-// import Mobilenavbar from "./components/navbar/MobileNavbar";
-// import Navbar from "./components/navbar/Navbar";
-// import { useState } from "react";
-// import Type from "./components/typingtest/Type";
-import Stripe from "./stripecomponent/pages/Stripe";
-
+import Covid from "./15_apps/covidTracker/Covid";
 function App() {
-  return (
-        <Stripe />
-  );
+  return <Covid />;
 }
 
 export default App;
